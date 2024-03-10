044ed260d350c1b043ff91c0cc76ce4eff6de153
Toluwalope Oluyipe <kingtoluwalope@gmail.com>
2024-03-10 11:06:07 -0400

    Initial Commit

---
+++ MDEV1001-ASSIGNMENT-2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MDEV1001-ASSIGNMENT-2
+//
+//  Created by Blaze on 10/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MDEV1001-ASSIGNMENT-2/MDEV1001_ASSIGNMENT_2App.swift
@@ -0,0 +1,17 @@
+//
+//  MDEV1001_ASSIGNMENT_2App.swift
+//  MDEV1001-ASSIGNMENT-2
+//
+//  Created by Blaze on 10/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MDEV1001_ASSIGNMENT_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
